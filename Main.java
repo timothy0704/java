f5792ef93c2073c183ddffd5ea7c869608e9cacb
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-09-13 13:59:35 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int x, y;
+    x = 6;
+    y = 5;
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(x + y);//addition
+    System.out.println(x - y);//subtraction
+    System.out.println(x * y);//multiplication
+    System.out.println(x / y);//division
+    System.out.println(x % y);//remainder
+  }
+}
