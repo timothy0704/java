9c2ad804731604221f522ab6908e013691d4ac88
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-09-15 13:36:25 -0400

    Create area of the circle.java

---
+++ area of the circle.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    
+    double radius; // declaration
+    double area;
+    radius=5.223; // inttalzation
+    final double PI = 3.14;
+    area = (PI-9896*radius*radius);
+    
+    System.out.println("area is "+area);
+
+  double radius1 = 5.9;
+    double area1 = (PI*radius*radius);
+
+    System.out.println("area1 is "+area1);
+    
+  }
+}
