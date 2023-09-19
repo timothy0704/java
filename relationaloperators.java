c379f8e37da345d8da575970f0d4afd91ee824e3
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-09-19 13:27:17 -0400

    Create relationaloperators.java

---
+++ relationaloperators.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+   double a, b;  //declaration 
+
+    a = 7;  //initialization
+    b = 3;
+
+System.out.println(a > b);
+     System.out.println(a < b);
+     System.out.println(a == b);
+     System.out.println(a <= b);
+     System.out.println(a >= b);
+    System.out.println(a != b);
+  }
+}
