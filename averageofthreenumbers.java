3f208c46ce559dc69d967636547ce64abe447227
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-09-21 13:37:36 -0400

    Create averageofthreenumbers.java

---
+++ averageofthreenumbers.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+
+double num1 = 13;
+
+  double num2 = 12.5;
+
+  double num3 = 11.3;
+
+    
+double average = (num1 + num2 + num3)/3;
+
+    
+    System.out.println("average = " + average);
+  }
+}
