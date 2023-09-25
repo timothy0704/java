8176b118872bd058ee34305cadea7eb7a61b131a
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-09-25 14:01:59 -0400

    Create ifstatment.java

---
+++ ifstatment.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int a = 3;
+    int b = 2;
+
+    if(a==3){
+       System.out.println("a is equal to 3");
+    }
+    else{
+      System.out.println("a is not equal to 3");
+    }
+   
+  }
+} 
