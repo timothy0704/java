e885f2a75291187e372730c7bfbfaa1cee0fcd6f
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-09-27 13:32:44 -0400

    Create if statement3.java

---
+++ if statement3.java
@@ -0,0 +1,38 @@
+/* class Main {
+  public static void main(String[] args) {
+
+int n = 3;
+
+    if(n%2==0){
+       System.out.println(n + " is an even number");
+    }
+
+    else{
+       System.out.println(n + " is an odd number");
+    }
+   
+  }
+}  */
+class Main {
+  public static void main(String[] args) {
+
+    int s = 25;
+
+    
+    if(s>30)
+     {
+        System.out.println("The number is bigger than 30");
+     }
+         if(s>20)
+          {
+            System.out.println("The number is bigger than 20");
+          }
+              if(s>10)
+               {
+                System.out.println("The number is bigger than 10");
+               }
+
+  }
+}
+
+ 
