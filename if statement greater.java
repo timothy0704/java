8c902b04567faf7ad9d16258c88187a18153a5ae
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-09-27 13:00:26 -0400

    Create if statement greater.java

---
+++ if statement greater.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int a = 3, b = 4; // initialization and declaration 
+
+
+    if(a>b)// if elements
+    { //true statement 
+       System.out.println("a is greater than b");
+    } 
+    else// else elements
+    { //false elements
+      System.out.println("a is greater than b");
+    }
+   
+  }
+} 
