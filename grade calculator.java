e39adcda6a14e29ec9d9d8553c8f4fa52b75ff31
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-10-05 13:01:58 -0400

    Create grade calculator.java

---
+++ grade calculator.java
@@ -0,0 +1,23 @@
+
+class Main {
+  public static void main(String[] args) {
+
+int n = 95;
+    if(n>=90){
+      System.out.println("The grade is A");
+    }
+      else if(n>=80){
+        System.out.println("The grade is B");
+      }
+        else if(n>=70){
+          System.out.println("The grade is C");
+        }
+          else if(n>=60){
+          System.out.println("The grade is D");
+        }
+            else if(n<=50){
+          System.out.println("The grade is F");
+        }
+
+  }
+}
