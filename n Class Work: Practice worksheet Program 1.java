052d1e746e9aa463cbd555976aa0a0aca983afbb
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-10-10 13:20:36 -0400

    Create n Class Work: Practice worksheet Program 1.java

---
+++ n Class Work: Practice worksheet Program 1.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("oOOO()");
+     System.out.println(" / _)");
+    System.out.println(" |  (");
+    System.out.println(" \\__)  ()OOOo");
+    System.out.println("        (_  \\");
+    System.out.println("          ) |");
+    System.out.println("oOOO()    (__/");
+    System.out.println(" / _)");
+    System.out.println(" |  (");
+    System.out.println(" \\__)  ()OOOo");
+    System.out.println("        (_  \\");
+    System.out.println("          ) |");
+    System.out.println("          (__/");
+  }
+}
