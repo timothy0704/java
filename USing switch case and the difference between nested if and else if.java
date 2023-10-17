df22378b2978d7c19666ab67cc13499f03327202
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-10-17 13:16:48 -0400

    Create USing switch case and the difference between nested if and else if.java

---
+++ USing switch case and the difference between nested if and else if.java
@@ -0,0 +1,41 @@
+class Main {
+  public static void main(String[] args) {
+
+   /* int x = 90; 
+
+   /* if(x >90){
+      System.out.println("Hello world!");
+    }
+
+    else if(x >80){
+      System.out.println("Hello world!");
+    } 
+
+    switch(x){
+      case 90:
+    System.out.println("90");
+    break;
+      case 80:
+         System.out.println("80");
+        break;
+    } */
+
+    char x = 'r';
+
+    switch(x){
+        case '1':
+        System.out.println("90");
+        break;
+        case '2':
+        System.out.println("80");
+        break;
+        case '3':
+        System.out.println("70");
+        break;
+        default:
+        System.out.println("cases other than, 1, 2, and 3");
+        break;
+    }
+    
+  }
+}
